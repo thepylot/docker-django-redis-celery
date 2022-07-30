cd9793475172b348ccc6e82e930d501a16ea7ac9
Niceiyke <oyomworld@gmail.com>
2022-07-30 20:37:40 +0100

    tt

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l^)6&tgav8q$#g^aj7au^b=^*l98%c#9_v^85gu!%-s$bq7%(2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["44.208.163.176"]
+ALLOWED_HOSTS = ["44.208.163.176","54.174.123.141"]
 
 
 # Application definition

91e878fc04528955be42a62c4757633959f2f5f9
Niceiyke <oyomworld@gmail.com>
2022-07-30 19:30:03 +0100

    settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

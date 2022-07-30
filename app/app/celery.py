0690c4010c18c56e652a4bd13e2a1d7b4ba9507b
Niceiyke <oyomworld@gmail.com>
2022-07-30 09:07:24 +0100

    de

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,8 +1,19 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-app = Celery('app')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','core.settings')
+
+
+app =Celery('core')
+
+app.config_from_object('django.conf:settings',namespace='CELERY')
+
+app.conf.beat_schedule ={
+    'scrape_result':{
+        'task':'scrape.tasks.ScrapeResult',
+        'schedule': 600.0
+    }
+}
+
 app.autodiscover_tasks()

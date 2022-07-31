b58e39c5ee54cf0f0f08972085c41a04a9efe7f5
Niceiyke <oyomworld@gmail.com>
2022-07-31 11:55:01 +0100

    kkk

--- app/app/celery.py
+++ app/app/celery.py
@@ -12,7 +12,7 @@ app.config_from_object('django.conf:settings',namespace='CELERY')
 app.conf.beat_schedule ={
     'scrape_result':{
         'task':'core.tasks.ScrapeResult',
-        'schedule': 60.0
+        'schedule': 600.0
     }
 }
 
